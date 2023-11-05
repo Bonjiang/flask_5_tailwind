297fdad666a0636086a0860076cc2c5cf4fe2c51
bonjiang <bonniejiang1@gmail.com>
2023-11-05 23:55:11 +0000

    updated

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index_tailwind.html')
+
+@app.route('/about')
+def aboutpage():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(
+        debug=True,
+        port=8080
+    )
